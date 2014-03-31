f140cb597379faf15539b2c2d8811285f6a07c75
Erin Bush <erinbush@live.com>
2014-03-31 15:56:49 -0700

    fixed merge conflicts -- should work

--- CS304/src/library/ClerkModel.java
+++ CS304/src/library/ClerkModel.java
@@ -10,12 +10,10 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Types;
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
-import java.util.Date;
 
 public class ClerkModel {
 	
@@ -236,7 +234,7 @@ public class ClerkModel {
 		int borid = 0;
 		try{
 			stmt = con.createStatement();
-			rs = stmt.executeQuery("SELECT bid, borid, outDate FROM borrowing WHERE callnumber = "+ callNumber + " and copyNo = " + copyNo + " and indate is null");
+			rs = stmt.executeQuery("SELECT bid, borid, outDate FROM borrowing WHERE callnumber = "+ callNumber + " and copyNo = " + copyNo);
 			
 			while(rs.next()){
 
@@ -261,7 +259,7 @@ public class ClerkModel {
 		String dueDate = ComputeDueDate(bid, outDate, con);
 		System.out.println("Due Date : " + dueDate);
 		
-		UpdateStatusIn(callNumber, copyNo, con);
+		UpdateStatusIn(callNumber, copyNo,con);
 		UpdateInDate(inDate, callNumber, con);
 		
 		
@@ -318,7 +316,7 @@ public class ClerkModel {
 			
 			//check hold requests
 				
-			CheckHoldRequests(callNumber, copyNo, con);
+			CheckHoldRequests(callNumber, con);
 			
 			
 		}
@@ -330,7 +328,7 @@ public class ClerkModel {
 	
 	
 	
-	private void UpdateStatusIn(String callNumber, String copyNo, Connection con) {
+	private void UpdateStatusIn(String callNumber, String copyNo,Connection con) {
 		PreparedStatement ps;
 		
 		try {
@@ -339,10 +337,9 @@ public class ClerkModel {
 		
 			ps.setString(1, callNumber);
 			ps.setString(2, copyNo);
-			System.out.println("callnumber : "+ callNumber + " copy no: " + copyNo);
 			
 			ps.executeUpdate();
-			con.commit();
+		
 			ps.close();
 		}
 		catch(SQLException e) {
@@ -351,15 +348,15 @@ public class ClerkModel {
 		}
 	}
 	
-	private void UpdateStatusHold(String callNumber, String copyNo, Connection con) {
+	private void UpdateStatusHold(String callNumber, Connection con) {
 		PreparedStatement ps;
 		
 		try {
 			
-			ps = con.prepareStatement("UPDATE bookcopy SET status = 'on-hold' WHERE callNumber = ? and copyNo = ?");
+			ps = con.prepareStatement("UPDATE bookcopy SET status = 'on-hold' WHERE callNumber = ? and status = ?");
 			
 			ps.setString(1, callNumber);
-			ps.setString(2, copyNo);
+			ps.setString(2, "out");
 			
 			ps.executeUpdate();
 			
@@ -381,7 +378,7 @@ public class ClerkModel {
 			ps.setString(2, callNumber);
 			
 			ps.executeUpdate();
-			con.commit();
+			
 			ps.close();
 			}
 			catch(SQLException e) {
@@ -390,7 +387,7 @@ public class ClerkModel {
 			}
 	}
 	
-	private void CheckHoldRequests(String callNumber, String copyNo, Connection con) {
+	private void CheckHoldRequests(String callNumber, Connection con) {
 		ResultSet rs;
 		Statement stmt;
 		List<String> issuedDates = new ArrayList<String>();
@@ -409,22 +406,12 @@ public class ClerkModel {
 		    
 		    //CHECK WHO RESERVED THE BOOK FIRST AND EMAIL THEM
 		    if (isHold){
-		    	Integer firstBID = firstReserve(issuedDates, bid);
+		    	Integer first = firstReserve(issuedDates, bid);
 		    
-		    	UpdateStatusHold(callNumber, copyNo, con); 
-		    	//delete hold request
-		    	
-		    	ps = con.prepareStatement("DELETE FROM holdrequest WHERE callnumber = ? and bid = ?");
-		    	
-		    	ps.setString(1, callNumber);
-		    	ps.setInt(2, firstBID);
-		    	
-		    	ps.executeUpdate();
-				con.commit();
-				ps.close();
-		    	
+		    	UpdateStatusHold(callNumber, con); 
+		    
 		    	System.out.println("BOOK PLACED ON HOLD");
-		    	System.out.println("EMAILING BID USER: "+ firstBID);
+		    	System.out.println("EMAILING BID USER: "+ first);
 		    
 		    }
 		}
@@ -529,12 +516,7 @@ public class ClerkModel {
 					
 	}
 
-<<<<<<< HEAD
 	private void AssessFine(Integer bid, String callNumber, Integer fid, double amount, String issuedDate, Connection con) {
-=======
-//###################################### Caution ########################################	
-	private void AssessFine(Integer bid, String callNumber, Integer fid, double amount, Date issuedDate, Connection con) {
->>>>>>> ccc05e9b0ca1650a85d7e621ea4e5a2319f1b5e5
 		try {
 			ps = con.prepareStatement("SELECT borid FROM borrowing WHERE bid = ?, callnumber = ?");
 			
@@ -555,11 +537,7 @@ public class ClerkModel {
 			
 			ps.setInt(1, fid);
 			ps.setDouble(2, amount);
-<<<<<<< HEAD
 			ps.setString(3, issuedDate);
-=======
-			//ps.setDate(3, issuedDate); // THIS NEED TO BE CHANGED
->>>>>>> ccc05e9b0ca1650a85d7e621ea4e5a2319f1b5e5
 			ps.setNull(4, Types.DATE);
 			ps.setInt(5, borid);
 			
@@ -649,60 +627,37 @@ public class ClerkModel {
 			return null;
 		}
 	}
+	
 	private boolean isOverdue(String inDate, String dueDate){
-<<<<<<< HEAD
-
-		Date inDate_Date, dueDate_Date;
-
-		inDate_Date = stringToDate(inDate);
-		dueDate_Date = stringToDate(dueDate);
-
-=======
 		
 		Date inDate_Date, dueDate_Date;
 		
 		inDate_Date = stringToDate(inDate);
 		dueDate_Date = stringToDate(dueDate);
 		
->>>>>>> ccc05e9b0ca1650a85d7e621ea4e5a2319f1b5e5
 		//inDate_Date is after dueDate_Date
 		if(inDate_Date.compareTo(dueDate_Date) > 0) 
 			return true;
 		else
 			return false;
 	}
-
+	
 	private String AdjustDate(String outDate, int length) {
 		Date temp, result;
 		String output;
-<<<<<<< HEAD
-
-		temp = stringToDate(outDate);
-		result = addDate(temp, length*7 );
-		output = dateToString(result);
-
-		return output;
-	}
-
-=======
 		
 		temp = stringToDate(outDate);
-		result = addDate(temp, length);
+		result = addDate(temp, length*7 );
 		output = dateToString(result);
 		
 		return output;
 	}
 	
->>>>>>> ccc05e9b0ca1650a85d7e621ea4e5a2319f1b5e5
 	private Date stringToDate(String string_input)
 	{
 		SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy");
 		Date date_output = null;
-<<<<<<< HEAD
-
-=======
 		
->>>>>>> ccc05e9b0ca1650a85d7e621ea4e5a2319f1b5e5
 		try
 		{
 			date_output = df.parse(string_input);
@@ -711,35 +666,6 @@ public class ClerkModel {
 		{
 			e.printStackTrace();
 		}
-<<<<<<< HEAD
-
-		return date_output;
-	}
-
-	private String dateToString(Date date_input) {
-
-		String string_output = null;
-
-		DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
-		string_output = df.format(date_input);
-
-		return string_output;
-	}
-
-	private Date addDate(Date inputDate, int amount_add) {
-
-		Date result;
-
-		Calendar c = Calendar.getInstance(); 
-		c.setTime(inputDate);
-
-		c.add(Calendar.DATE, amount_add);
-		result = c.getTime();
-
-		return result;
-	}
-
-=======
 		
 		return date_output;
 	}
@@ -766,7 +692,5 @@ public class ClerkModel {
 		
 		return result;
 	}
-		
->>>>>>> ccc05e9b0ca1650a85d7e621ea4e5a2319f1b5e5
+	
 }
-
